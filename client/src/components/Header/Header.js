7001a8abc7dacd6d30568f1ef86872b1e6a59866
Nikolay Petrov <nikip1303@gmail.com>
2023-05-02 12:53:56 +0300

    Header fixed

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,7 +1,11 @@
+import { useContext } from 'react'
+import { AuthContext } from '../../context/AuthContext'
 import style from './header.module.css'
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
 export const Header = () => {
+    const { user } = useContext(AuthContext);
+
     return (
         <header className={style["nav-bar"]}>
             <div>
@@ -10,20 +14,32 @@ export const Header = () => {
             <div className={style["navigation"]}>
                 <ul className={style["nav-list"]}>
                     <li>
-                    <Link className={style["links"]} to="">Home</Link>
+                        <Link className={style["links"]} to="">Home</Link>
                     </li>
-                    <li>
-                    <a className={style["links"]} href="">Profile</a>
-                    </li>
-                    <li>
-                    <Link className={style["links"]} to="/login">Login</Link>
-                    </li>
-                    <li>
-                    <a className={style["links"]} href="">Logout</a>
-                    </li>
-                    <li>
-                    <a className={style["links"]} href="">Register</a>
-                    </li>
+                    {user?.accessToken ? (
+                        <>
+                            <li>
+                                <a className={style["links"]} href="">Profile</a>
+                            </li>
+                            <li>
+                                <a className={style["links"]} href="">Logout</a>
+                            </li>
+                        </>
+
+                    ) : (
+                        <>
+                            <li>
+                                <Link className={style["links"]} to="/login">Login</Link>
+                            </li>
+
+                            <li>
+                                <a className={style["links"]} href="">Register</a>
+                            </li>
+                        </>
+
+                    )}
+
+
                 </ul>
             </div>
         </header>
